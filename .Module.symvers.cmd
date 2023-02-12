5bcf3aa01eff9690da4a4662c73aee08c346b2e3
loquat <1635784842@qq.com>
2023-02-12 20:21:30 +0800

    mpt3sas

--- .Module.symvers.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/loquat/mpt3sas/Module.symvers := sed 's/\.ko$$/\.o/' /home/loquat/mpt3sas/modules.order | scripts/mod/modpost -m -a  -o /home/loquat/mpt3sas/Module.symvers -e -i Module.symvers   -T -
--- .modules.order.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/loquat/mpt3sas/modules.order := {   echo /home/loquat/mpt3sas/mpt3sas_1.ko; :; } | awk '!x[$$0]++' - > /home/loquat/mpt3sas/modules.order
